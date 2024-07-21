0f18b1098790ee8791d0fed9280a5c7824f6e758
arthur <arthurregiani@gmail.com>
2024-07-21 18:46:58 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/ifsp/dmo/pedratesouraepapel/view/MainActivity.kt
@@ -0,0 +1,54 @@
+package br.edu.ifsp.dmo.pedratesouraepapel.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.dmo.pedratesouraepapel.R
+import br.edu.ifsp.dmo.pedratesouraepapel.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        configToolBar()
+        configSpinner()
+        configListener()
+    }
+
+    private fun configListener() {
+        binding.buttonStart.setOnClickListener { startGame() }
+    }
+
+    private fun configSpinner() {
+        val adapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_spinner_dropdown_item,
+            resources.getStringArray(R.array.tipos_jogos)
+        )
+        binding.spinnerButtles.adapter = adapter
+    }
+
+    private fun configToolBar() {
+        supportActionBar?.hide()
+    }
+
+    private fun startGame() {
+        val battles: Int = when (binding.spinnerButtles.selectedItemPosition) {
+            0 -> 1
+            1 -> 3
+            else -> 5
+        }
+        val isBotPlayer2 = binding.checkBoxBotPlayer2.isChecked
+        val mIntent = Intent(this, WarActivity::class.java)
+        mIntent.putExtra(Constants.KEY_PLAYER_1, binding.edittextPlayer1.text.toString())
+        mIntent.putExtra(Constants.KEY_PLAYER_2, binding.edittextPlayer2.text.toString())
+        mIntent.putExtra(Constants.KEY_ROUNDS, battles)
+        mIntent.putExtra(Constants.KEY_IS_BOT_PLAYER_2, isBotPlayer2)
+        startActivity(mIntent)
+    }
+}
+
